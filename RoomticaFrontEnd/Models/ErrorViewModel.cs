c3d90454607d740e3455a03b1c9b754f74aed2fe
Frank <fcarpio113@gmail.com>
2025-04-12 23:50:20 -0500

    Agregar archivos de proyecto.

---
+++ RoomticaFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoomticaFrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
